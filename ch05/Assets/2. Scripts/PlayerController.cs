2a7989e44f122845ab56c7a165fe9bd2dca03c87
Amapo057 <jiseong2002@naver.com>
2025-04-16 14:19:27 +0900

    ch05 end

--- ch05/Assets/2. Scripts/PlayerController.cs
+++ ch05/Assets/2. Scripts/PlayerController.cs
@@ -25,4 +25,13 @@ public class PlayerControlar : MonoBehaviour
             transform.Translate(moveStep, 0, 0);
         }
     }
+    public void LButtonDown()
+    {
+        transform.Translate(-moveStep, 0, 0);
+    }
+
+    public void RButtonDown()
+    {
+        transform.Translate(moveStep, 0, 0);
+    }
 }
