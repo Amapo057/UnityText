3cc375b4c627d6698a0dbfd4a1dea7e3ad1fa83a
Amapo057 <jiseong2002@naver.com>
2025-04-16 13:07:26 +0900

    add dodge

---
+++ ch05/Assets/Dodge/DodgePlayer.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgePlayer : MonoBehaviour
+{
+    public float moveStep = 0.3f;
+    int moveDirection = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            moveDirection -= 1;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            moveDirection += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) 
+        {
+            moveDirection += 1;
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            moveDirection -= 1;
+        }
+        transform.Translate(moveStep * moveDirection, 0, 0);
+    }
+}
